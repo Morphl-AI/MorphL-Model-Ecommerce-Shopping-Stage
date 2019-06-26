6a3f911006a15f7460e18f513af137e32397a502
Alexandra Anghel <alexandra@appticles.com>
2019-06-26 12:51:06 +0300

    Set permissions

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,6 +1,7 @@
 echo 'Installing the Shopping Stage Prediction module...'
 echo
 
+# Create file with env variables for the module
 touch /home/airflow/.morphl_ga_epna_environment.sh
 chmod 660 /home/airflow/.morphl_ga_epna_environment.sh
 chown airflow /home/airflow/.morphl_ga_epna_environment.sh
@@ -9,6 +10,13 @@ truncate -s 0 /home/airflow/.morphl_ga_epna_environment.sh
 echo "export GA_EPNA_KEY_FILE_LOCATION=/opt/secrets/ga_epna/gcloud_service_account.json" >> /home/airflow/.morphl_ga_epna_environment.sh
 echo "export GA_EPNA_VIEW_ID=\$(</opt/secrets/ga_epna/viewid.txt)" >> /home/airflow/.morphl_ga_epna_environment.sh
 
+# Add credentials to airflow profile
 if ! grep -q "/home/airflow/.morphl_ga_epna_environment.sh" /home/airflow/.profile; then
   echo ". /home/airflow/.morphl_ga_epna_environment.sh" >> /home/airflow/.profile
 fi
+
+# Set file permissions
+chmod -R 775 /opt/secrets/ga_epna
+chmod 660 /opt/secrets/ga_epna/viewid.txt
+chmod 660 /opt/secrets/ga_epna/gcloud_service_account.json
+chgrp airflow /opt/secrets/ga_epna /opt/secrets/ga_epna/gcloud_service_account.json /opt/secrets/ga_epna/viewid.txt
