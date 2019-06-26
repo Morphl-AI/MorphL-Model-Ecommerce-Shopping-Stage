129e16a1a2bbd09806927217b162e828d80ba5f3
Alexandra Anghel <alexandra@appticles.com>
2019-06-26 12:53:06 +0300

    Rename service account file

--- setup-environment.sh
+++ setup-environment.sh
@@ -7,7 +7,7 @@ chmod 660 /home/airflow/.morphl_ga_epna_environment.sh
 chown airflow /home/airflow/.morphl_ga_epna_environment.sh
 truncate -s 0 /home/airflow/.morphl_ga_epna_environment.sh
 
-echo "export GA_EPNA_KEY_FILE_LOCATION=/opt/secrets/ga_epna/gcloud_service_account.json" >> /home/airflow/.morphl_ga_epna_environment.sh
+echo "export GA_EPNA_KEY_FILE_LOCATION=/opt/secrets/ga_epna/service_account.json" >> /home/airflow/.morphl_ga_epna_environment.sh
 echo "export GA_EPNA_VIEW_ID=\$(</opt/secrets/ga_epna/viewid.txt)" >> /home/airflow/.morphl_ga_epna_environment.sh
 
 # Add credentials to airflow profile
@@ -18,5 +18,5 @@ fi
 # Set file permissions
 chmod -R 775 /opt/secrets/ga_epna
 chmod 660 /opt/secrets/ga_epna/viewid.txt
-chmod 660 /opt/secrets/ga_epna/gcloud_service_account.json
-chgrp airflow /opt/secrets/ga_epna /opt/secrets/ga_epna/gcloud_service_account.json /opt/secrets/ga_epna/viewid.txt
+chmod 660 /opt/secrets/ga_epna/service_account.json
+chgrp airflow /opt/secrets/ga_epna /opt/secrets/ga_epna/service_account.json /opt/secrets/ga_epna/viewid.txt
