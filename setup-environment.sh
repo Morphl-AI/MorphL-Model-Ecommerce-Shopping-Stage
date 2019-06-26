08832e1265779d16743e76f899c52a02166aa9f6
Alexandra Anghel <alexandra@appticles.com>
2019-06-26 12:40:03 +0300

    Overwrite existing env variables instead of appending

--- setup-environment.sh
+++ setup-environment.sh
@@ -4,7 +4,11 @@ echo
 touch /home/airflow/.morphl_ga_epna_environment.sh
 chmod 660 /home/airflow/.morphl_ga_epna_environment.sh
 chown airflow /home/airflow/.morphl_ga_epna_environment.sh
+truncate -s 0 /home/airflow/.morphl_ga_epna_environment.sh
 
 echo "export GA_EPNA_KEY_FILE_LOCATION=/opt/secrets/ga_epna/gcloud_service_account.json" >> /home/airflow/.morphl_ga_epna_environment.sh
 echo "export GA_EPNA_VIEW_ID=\$(</opt/secrets/ga_epna/viewid.txt)" >> /home/airflow/.morphl_ga_epna_environment.sh
-echo ". /home/airflow/.morphl_ga_epna_environment.sh" >> /home/airflow/.profile
+
+if ! grep -q "/home/airflow/.morphl_ga_epna_environment.sh" /home/airflow/.profile; then
+  echo ". /home/airflow/.morphl_ga_epna_environment.sh" >> /home/airflow/.profile
+fi
