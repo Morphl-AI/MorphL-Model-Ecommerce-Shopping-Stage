f2c05e64f957aace1017ad42a4bd9880ea2a2bf9
Alexandra Anghel <alexandra@appticles.com>
2019-07-01 14:02:09 +0300

    Fix batch inference path

--- prediction/batch_inference/runbatchinference.sh
+++ prediction/batch_inference/runbatchinference.sh
@@ -1,5 +1,5 @@
 cp -r /opt/ga_epna /opt/code
 cd /opt/code
 
-spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar /opt/ga_epna/prediction/batch_inference/ga_epna_batch_inference.py
+spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar /opt/code/prediction/batch_inference/ga_epna_batch_inference.py
 
