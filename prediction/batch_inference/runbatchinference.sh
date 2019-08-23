47216fd5e0fd9e1c1c58fdf54239a7dc7c93b58a
abarbulescu <alexandru@appticles.com>
2019-08-23 13:00:34 +0300

    Increase driver memory

--- prediction/batch_inference/runbatchinference.sh
+++ prediction/batch_inference/runbatchinference.sh
@@ -1,5 +1,5 @@
 cp -r /opt/ga_epna /opt/code
 cd /opt/code
 
-spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar /opt/code/prediction/batch_inference/ga_epna_batch_inference.py
+spark-submit --driver-memory 4g --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar /opt/code/prediction/batch_inference/ga_epna_batch_inference.py
 
